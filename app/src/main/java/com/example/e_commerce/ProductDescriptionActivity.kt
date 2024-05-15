f08c929a24ca86da19a6019bb62a339e6ad0d2ed
Vikash kumar <Vraj67207@gmail.com>
2024-05-15 16:59:02 +0530

    "spinner implement"

--- app/src/main/java/com/example/e_commerce/ProductDescriptionActivity.kt
+++ app/src/main/java/com/example/e_commerce/ProductDescriptionActivity.kt
@@ -2,9 +2,15 @@ package com.example.e_commerce
 
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.e_commerce.databinding.ActivityProductDescriptionBinding
 import com.squareup.picasso.Picasso
@@ -18,16 +24,24 @@ class ProductDescriptionActivity : AppCompatActivity() {
         binding = ActivityProductDescriptionBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val size = resources.getStringArray(R.array.Quantity)
-        val arrayAdapter = ArrayAdapter(this,R.layout.menu_list,size)
 
+        val list = resources.getStringArray(R.array.Quantity)
+        val spinner : Spinner = findViewById(R.id.spinner_view)
+        spinner.adapter= ArrayAdapter(this,R.layout.menu_list,R.id.menu_listt,list)
 
-        binding.autoComplet.setAdapter(arrayAdapter)
+        spinner.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                Toast.makeText(this@ProductDescriptionActivity,list[position],Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
             val title = intent.getStringExtra("name")
             val imageUrl = intent.getStringExtra("images")
             val desc = intent.getStringExtra("description")
             val rating = intent.getFloatExtra("rating", 3F)
-
 
 
             binding.productId.text = title.toString()
