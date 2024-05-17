89cf000155eefff76ae295ac231f3b9701c0c24a
Vikash kumar <Vraj67207@gmail.com>
2024-05-17 09:47:41 +0530

    "if user will log out it will go on login page  "

--- app/src/main/java/com/example/e_commerce/Profile.kt
+++ app/src/main/java/com/example/e_commerce/Profile.kt
@@ -1,6 +1,8 @@
 package com.example.e_commerce
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,23 +15,43 @@ import com.google.firebase.auth.FirebaseAuth
 class Profile : Fragment() {
   private lateinit var binding: FragmentProfileBinding
   private lateinit var firebaseAuth: FirebaseAuth
+  private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         binding = FragmentProfileBinding.inflate(layoutInflater)
         firebaseAuth = FirebaseAuth.getInstance()
+        sharedPreferences = requireContext().getSharedPreferences("loginPrefs", Context.MODE_PRIVATE)
+
+
+
         binding.btnSignOut.setOnClickListener {
-            firebaseAuth.signOut()
-            startActivity(Intent(requireContext(), SignUp::class.java))
-            requireActivity().finish()
+           signOut()
 
 
         }
 
         return binding.root
+    }
+    private fun signOut() {
+        firebaseAuth.signOut()
+        setLoggedOut()
+        navigateToSignUpActivity()
+        requireActivity().finish()
+    }
+
+    private fun setLoggedOut() {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isLoggedIn", false)
+        editor.apply()
+    }
+
+    private fun navigateToSignUpActivity() {
+        val intent = Intent(requireContext(), Login::class.java)
+        startActivity(intent)
     }
 
 
